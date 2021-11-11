5f16a2f738b587db155a59057a8522ec7825097d
lukmanis25 <87930936+lukmanis25@users.noreply.github.com>
2021-11-11 16:11:27 +0100

    Update prolog_zad2.pl
    Dodanie tree sort

--- prolog_zad2.pl
+++ prolog_zad2.pl
@@ -1,11 +1,26 @@
-% Tu trzeba dać sortowanie drzewiaste (narazie jest jakies z neta)
-quick_sort(List,Sorted):-q_sort(List,[],Sorted).
-q_sort([],Acc,Acc).
-q_sort([H|T],Acc,Sorted):- pivoting(H,T,L1,L2),
-    q_sort(L1,Acc,Sorted1),q_sort(L2,[H|Sorted1],Sorted).
-pivoting(_,[],[],[]).
-pivoting(H,[X|T],[X|L],G):-X=<H,pivoting(H,T,L,G).
-pivoting(H,[X|T],L,[X|G]):-X>H,pivoting(H,T,L,G).
+dolacz( [],L,L ). 
+dolacz( [H|T],L,[H|X] ) :- dolacz( T,L,X ).
+
+add(X,nil,t(X,nil,nil)).
+add(X,t(Root,L,R),t(Root,L1,R)) :- X < Root, add(X,L,L1).
+add(X,t(Root,L,R),t(Root,L,R1)) :- X >= Root, add(X,R,R1).
+
+
+construct(L, T) :- construct(L,T,nil).
+
+construct([],T,T).
+construct([N|Ns],T,T0) :- add(N,T0,T1), construct(Ns,T,T1).
+
+traverse(nil, []).
+traverse(t(X, Left, Right), C) :- 
+	traverse(Left, R1),
+	traverse(Right, R2),
+	dolacz(R2, [X], N),
+	dolacz(N, R1, C).
+	%dolacz([X], R2, N),
+	%dolacz(R1, N, C).
+
+tree_sort(L, X) :- construct(L, T), traverse(T, X).
 
 %-	Ciag jest graficzny jesli jego podciag jest graficzny
 %-	Podciągiem jest ciąg z wykluczeniem największego wierzchołka n i z odjeciem od pierwych
@@ -23,14 +38,12 @@ czy_lista_nie_ma_ujemnej([H|T]) :- H>=0 , czy_lista_nie_ma_ujemnej(T) .
 dlugosc_listy([],0).
 dlugosc_listy([_|T],N) :- dlugosc_listy(T,N1), N is N1+1.
 
-dolacz( [],L,L ).
-dolacz( [H|T],L,[H|X] ) :- dolacz( T,L,X ).
-
 zmniejsz_n_ele_o_1([],_,[]).
 zmniejsz_n_ele_o_1(L,0,L).
 zmniejsz_n_ele_o_1([H|T],N,[X|W]) :- X is H-1,dec(N,N1), zmniejsz_n_ele_o_1(T,N1,W).
 
-minimalizuj([H|T], W) :-  quick_sort([H|T],[H1|T1]) ,dlugosc_listy(T1,N), N >= H ,
+minimalizuj([H|T], W) :-  
+    tree_sort([H|T],[H1|T1]) ,dlugosc_listy(T1,N), N >= H ,
     zmniejsz_n_ele_o_1(T1,H1,W), czy_lista_nie_ma_ujemnej(W).
 
 czy_minimalny([]).
